5530de2a81e2e3597aade2fdd65c31367b48f5bf
we <david@wemakeprice.com>
2019-01-16 17:56:32 +0900

    example hist_with_opt

---
+++ src/opt_ex.py
@@ -0,0 +1,35 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+import ot
+import ot.plot
+from ot.datasets import get_1D_gauss as gauss
+
+#%% parameters
+
+n = 100  # nb bins
+
+# bin positions
+x = np.arange(n, dtype=np.float64)
+
+# Gaussian distributions
+a = gauss(n, m=20, s=5)  # m= mean, s= std
+b = gauss(n, m=60, s=10)
+
+# loss matrix
+M = ot.dist(x.reshape((n, 1)), x.reshape((n, 1)))
+M /= M.max()
+
+#%% plot the distributions
+
+plt.figure(1, figsize=(6.4, 3))
+plt.plot(x, a, 'b', label='Source distribution')
+plt.plot(x, b, 'r', label='Target distribution')
+plt.legend()
+
+#%% plot distributions and loss matrix
+
+plt.figure(2, figsize=(5, 5))
+ot.plot.plot1D_mat(a, b, M, 'Cost matrix M')
+
+plt.show()
